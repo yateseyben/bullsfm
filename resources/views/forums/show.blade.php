ebd393d9a592045c3a1b856988ec6ee82246d321
Ben Yates <yateseyb@hotmail.com>
2018-08-27 18:38:39 +0100

    Initial table area for forum posts

--- resources/views/forums/show.blade.php
+++ resources/views/forums/show.blade.php
@@ -7,9 +7,30 @@
 
 <a href="{{ route('forums.createTopic', $forum->id) }}">Create A New Topic</a>
 
-<div class = "topic-list">
-	@foreach($topics as $topic)
-	<a href = "{{ route('topics.show', $topic->id) }}">{{ $topic->title }}</a>
-	@endforeach
+<div class = "topic-list listings">
+	<table>
+		<thead>
+			<th width ="60%">Name</th>
+			<th width ="10%">Posts</th>
+			<th width ="30%">Last Update</th>
+		</thead>
+		@foreach($topics as $topic)
+		<tr class="clickable-row" data-href="{{ route('topics.show', $topic->id) }}">
+			<tbody>
+				<td><a href="{{ route('topics.show', $topic->id) }}">{{ $topic->getTitle() }}</a></td>
+				<td>{{ $topic->posts()->count() }}</td>
+				<td>{{ \Carbon\Carbon::parse($topic->posts()->max('created_at'))->format('d M Y g:i a') }}</td>	
+			</tbody>
+		</tr>
+		@endforeach
 </div>
 @endsection
+<script>
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
+
+</script>
